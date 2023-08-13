77736ea834ab16ee7846d18bf43dd729dddca0ca
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2023-08-13 17:23:31 +0000

    Deploying to gh-pages from @ Nazar-Odno/goit-js-hw-08_test@f9187b1aa7435a6fccfb1efd126e1b97825765a8 🚀

--- 02-video.059840f6.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),t=new Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e))}),1e3));try{const{seconds:e}=JSON.parse(localStorage.getItem("videoplayer-current-time"));t.setCurrentTime(e)}catch(e){console.log(e.name),console.log(e.message)}
-//# sourceMappingURL=02-video.059840f6.js.map
