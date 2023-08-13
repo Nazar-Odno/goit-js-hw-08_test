a2ce7f648732aa6b3f8c2dd51654adcdceec5987
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2023-08-13 18:17:28 +0300

    homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle(recordingCurrentTime, 1000));
+	
+try {
+	const {seconds} = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+	player.setCurrentTime(seconds);
+} catch (error) {
+	console.log(error.name);
+	console.log(error.message);
+};
+
+function recordingCurrentTime(data) { 
+	localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+}
