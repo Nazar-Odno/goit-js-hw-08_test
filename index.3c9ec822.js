9d70ce024459ff6c8004bb4381dd88bf42adc6a8
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2023-08-13 08:22:51 +0000

    Deploying to gh-pages from @ Nazar-Odno/goit-js-hw-08_test@6f4bf515bb8ab937d9d6b69c7b229e2faaf15b9a 🚀

---
+++ index.3c9ec822.js
@@ -0,0 +1,2 @@
+test="hello world",console.log(test);
+//# sourceMappingURL=index.3c9ec822.js.map
---
+++ index.e38f9a0a.js
@@ -0,0 +1,2 @@
+test="hello world",console.log("hello world");
+//# sourceMappingURL=index.e38f9a0a.js.map
